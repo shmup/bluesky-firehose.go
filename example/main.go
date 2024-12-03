87af4d3d212534a56f50c92f774c379ed5056726
Jared Miller <jared@smell.flowers>
2024-12-03 16:07:49 -0500

    Simplify main example implementation

--- example/main.go
+++ example/main.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 
-	bsky "github.com/shmup/bluesky-firehose.go" // Use alias
-
-	"github.com/bluesky-social/indigo/api/atproto"
+	bsky "github.com/shmup/bluesky-firehose.go"
 )
 
 func main() {
@@ -21,31 +18,13 @@ func main() {
 	}
 	godotenv.Load()
 
-	email := os.Getenv("BSKY_EMAIL")
-	password := os.Getenv("BSKY_PASSWORD")
-
-	err = firehose.Authenticate(email, password)
-	if err != nil {
+	if err := firehose.Authenticate(os.Getenv("BSKY_EMAIL"), os.Getenv("BSKY_PASSWORD")); err != nil {
 		log.Fatal(err)
 	}
 
-	handler := &PostHandler{firehose: firehose}
-	firehose.Subscribe(context.Background(), handler)
-}
+	firehose.OnPost(context.Background(), func(text string) error {
+		fmt.Printf("%s\n", text)
 
-type PostHandler struct {
-	firehose *bsky.Firehose
-}
-
-func (h *PostHandler) HandleEvent(evt *atproto.SyncSubscribeRepos_Commit) error {
-	for _, op := range evt.Ops {
-		if op.Action == "create" && strings.HasPrefix(op.Path, "app.bsky.feed.post") {
-			uri := fmt.Sprintf("at://%s/app.bsky.feed.post/%s",
-				evt.Repo, strings.TrimPrefix(op.Path, "app.bsky.feed.post/"))
-			if text, err := h.firehose.FetchPost(uri); err == nil {
-				fmt.Printf("New post: %s\n", text)
-			}
-		}
-	}
-	return nil
+		return nil
+	})
 }
